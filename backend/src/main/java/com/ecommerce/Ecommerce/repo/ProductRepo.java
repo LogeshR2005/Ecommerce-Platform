ca195ed00c95568d660d01782500fdb48a7c2387
Logesh R <142524635+LogeshR2005@users.noreply.github.com>
2025-09-28 22:26:12 +0530

    Add files via upload

---
+++ backend/src/main/java/com/ecommerce/Ecommerce/repo/ProductRepo.java
@@ -0,0 +1,22 @@
+package com.ecommerce.Ecommerce.repo;
+
+import com.ecommerce.Ecommerce.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepo extends JpaRepository<Product,Integer> {
+
+    //JPQL
+
+    @Query("SELECT p from Product p WHERE "
+            + "LOWER(p.name) LIKE LOWER(CONCAT('%',:keyword,'%')) OR " // space after OR
+            + "LOWER(p.description) LIKE LOWER(CONCAT('%',:keyword,'%')) OR " // space after OR
+            + "LOWER(p.brand) LIKE LOWER(CONCAT('%',:keyword,'%')) OR " // space after OR
+            + "LOWER(p.category) LIKE LOWER(CONCAT('%',:keyword,'%'))")
+    List<Product> searchProducts(String keyword);
+
+}
---
+++ backend/target/classes/data1.sql
@@ -0,0 +1,7 @@
+INSERT INTO Product (name, desc, brand, price, category,quantity, release_Date)
+VALUES
+('iPhone 16', 'Latest Apple smartphone', 'Apple', 79999.00, 'Mobile', 50, '2025-09-21'),
+('Galaxy S25', 'Flagship Samsung smartphone', 'Samsung', 74999.00, 'Mobile', 40, '2025-08-15'),
+('MacBook Air M4', 'Lightweight laptop with Apple M4 chip', 'Apple', 124999.00, 'Laptop', 20, '2025-07-01'),
+('Sony WH-1000XM6', 'Noise cancelling headphones', 'Sony', 29999.00, 'Headphones', 100, '2025-06-10'),
+('Nike Air Zoom', 'Running shoes with great comfort', 'Nike', 9999.00, 'Footwear', 200, '2025-05-05');
